49998df061d32ece4c7e0657ef26cc3fefb9ae19
caleb-lehman-02 <ca512755@ucf.edu>
2024-09-16 18:12:43 -0400

    reorganize project add controller support fix diagonal movement

--- prototype_character/prototype_character.gd
+++ prototype_character/prototype_character.gd
@@ -9,6 +9,9 @@ func _physics_process(delta: float) -> void:
 		Input.get_action_strength("down") - Input.get_action_strength("up"),
 	)
 	
+	if input_direction.length_squared() > 1:
+		input_direction = input_direction.normalized()
+	
 	velocity = input_direction * move_speed
 
 	move_and_slide()
